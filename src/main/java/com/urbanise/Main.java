3702c50fe76914098e45632f255ff3f842993de0
unknown <epnikolov@mail.com>
2022-11-23 20:28:10 +0200

    Remove hardcoded test

--- src/main/java/com/urbanise/Main.java
+++ src/main/java/com/urbanise/Main.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args) {
         if (args == null || args.length == 0) {
             System.out.println("You should supply at least one string argument.");
-//            return;
+            return;
         }
-
-        args = new String[]{"A B", "B C", "C A"};
 
         DependencyGraph dg = DependencyGraph.buildGraph(args);
         System.out.println("Display all dependencies for each item:");
